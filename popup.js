31a86ee4987b4be31b8ae4967796b691ce1e1f48
CosimaLagae <cosima.lagae@student.ehb.be>
2022-04-06 11:59:52 +0200

    Popup WIP

---
+++ popup.js
@@ -0,0 +1,18 @@
+function OpenModal() {
+    let element = document.getElementById('overons')
+    element.style.display = 'block'
+  }
+  function CloseModal() {
+    let element = document.getElementById('overons')
+    element.style.display = 'none'
+}
+
+function OpenGeraas() {
+    let element = document.getElementById('geraas')
+    element.style.display = 'block'
+  }
+  function CloseGeraas() {
+    let element = document.getElementById('close2')
+    element.style.display = 'none'
+}
+
