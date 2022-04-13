b784992a63d4a5b9d0c712a48d42c96c4bd5dfec
CosimaLagae <cosima.lagae@student.ehb.be>
2022-04-13 23:31:36 +0200

    First popup test overons

---
+++ popup.js
@@ -0,0 +1,8 @@
+function OpenModal() {
+    let element = document.getElementById('overons')
+    element.style.display = 'block'
+  }
+  function CloseModal() {
+    let element = document.getElementById('overons')
+    element.style.display = 'none'
+}
