a9b55fbf2de44c2f5b9865471041e5ccc0799396
cosimalagae <cosima.lagae@student.ehb.be>
2022-09-30 17:36:41 +0200

    Firebase deploy

--- popup.js
+++
@@ -1,8 +0,0 @@
-function OpenModal() {
-    let element = document.getElementById('overons')
-    element.style.display = 'block'
-  }
-  function CloseModal() {
-    let element = document.getElementById('overons')
-    element.style.display = 'none'
-}
